68436fb4ed6b51b7337ff8e7b4d25d65e28f7048
uzresk <uzr.esk@gmail.com>
2020-01-16 11:45:36 +0900

    apply material ui to Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,6 +1,18 @@
 import React from 'react';
+
 import {useForm} from "react-hook-form";
-import {MAXLENGTH, REQUIRED} from "../validation";
+
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Avatar from "@material-ui/core/Avatar";
+import Box from "@material-ui/core/Box";
+import {makeStyles} from "@material-ui/core";
+import LockIcon from "@material-ui/icons/Lock";
+import {Link} from "react-router-dom";
 
 const Signup = () => {
 
@@ -10,31 +22,115 @@ const Signup = () => {
         console.log(data);
     };
 
+    function Copyright() {
+        return (
+            <Typography variant="body2" color="textSecondary" align="center">
+                {'Copyright © '}
+                <a color="inherit" href="https://">
+                    your web site
+                </a>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        );
+    }
+
+    const useStyles = makeStyles(theme => ({
+        paper: {
+            marginTop: theme.spacing(8),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        avatar: {
+            margin: theme.spacing(1),
+            backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+            width: '100%', // Fix IE 11 issue.
+            marginTop: theme.spacing(3),
+        },
+        submit: {
+            margin: theme.spacing(3, 0, 2),
+        },
+    }));
+
+    const classes = useStyles();
+
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                名前:
-                <input type="text" name="userId" ref={register({required: true, maxLength: 20})}/>
-                {errors.userId && errors.userId.type === REQUIRED && 'User ID is required'}
-                {errors.userId && errors.userId.type === MAXLENGTH && 'Your input exceed maxLength'}
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <form className={classes.form} noValidate onSubmit={handleSubmit(onSubmit)}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField id="userId"
+                                       label="User ID"
+                                       name="userId"
+                                       inputRef={register({required: true, maxLength: 20})}
+                                       error={!!errors.userId}
+                                       helperText={errors.userId ? '20文字以内で入力してください' : ''}
+                                       variant="outlined"
+                                       fullWidth
+                                       autoFocus
+                                       required
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField id="email"
+                                       label="Email Address"
+                                       name="email"
+                                       inputRef={register({required: true})}
+                                       error={!!errors.email}
+                                       helperText={errors.email ? '必ず入力してください' : ''}
+                                       variant="outlined"
+                                       fullWidth
+                                       autoComplete="email"
+                                       required
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField id="password"
+                                       label="Password"
+                                       name="password"
+                                       type="password"
+                                       inputRef={register({required: true})}
+                                       error={!!errors.password}
+                                       helperText={errors.password ? '必ず入力してください' : ''}
+                                       variant="outlined"
+                                       fullWidth
+                                       autoComplete="password"
+                                       required
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            disabled={!formState.dirty || formState.isSubmitting}>
+                        Sign Up
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Link to="/" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
             </div>
-            <div>
-                メールアドレス
-                <input type="text" name="email" ref={register({required: true})}/>
-                {errors.email && errors.email.type === REQUIRED && 'Mail is required'}
-            </div>
-            <div>
-                パスワード
-                <input type="text" name="password" ref={register({required: true})}/>
-                {errors.password && errors.password.type === REQUIRED && 'Password is required'}
-            </div>
-            <div>
-                パスワード（確認用）
-                <input type="text" name="confirmPassword" ref={register({required: true})}/>
-                {errors.password && errors.password.type === REQUIRED && 'Password is required'}
-            </div>
-            <button disabled={!formState.dirty || formState.isSubmitting}>Submit</button>
-        </form>
+            <Box mt={5}>
+                <Copyright/>
+            </Box>
+        </Container>
     );
 };
 
